fac091d162cb4d9b6fb2d0e85c5d7e7d9fc59cd3
Mohamed Darwish <msdarwish@mayakapps.com>
2024-12-13 02:00:22 +0300

    fix: Important fix for performance

--- composeApp/src/androidMain/kotlin/org/example/lightsensormonitor/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/lightsensormonitor/MainActivity.kt
@@ -7,13 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val lightSensorMonitor = AndroidLightSensorMonitor(LocalContext.current)
+            val context = LocalContext.current
+            val lightSensorMonitor = remember { AndroidLightSensorMonitor(context) }
             CompositionLocalProvider(LocalLightSensorMonitor provides lightSensorMonitor) {
                 App()
             }
--- composeApp/src/iosMain/kotlin/org/example/lightsensormonitor/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/lightsensormonitor/MainViewController.kt
@@ -2,9 +2,10 @@ package org.example.lightsensormonitor
 
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.runtime.remember
 
 fun MainViewController() = ComposeUIViewController {
-    val lightSensorMonitor = IosLightSensorMonitor()
+    val lightSensorMonitor = remember { IosLightSensorMonitor() }
     CompositionLocalProvider(LocalLightSensorMonitor provides lightSensorMonitor) {
         App()
     }
